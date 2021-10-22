6aa05fb69e1d9636485116c2ef5d0844e11668d8
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2021-10-21 23:53:09 -0400

    Installed Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   mode: 'development',
 
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Look for files that end in .js
+        exclude: /node_modules/, // Exclude node_modules
+        use: {
+          loader: 'babel-loader' // Look for babel config file
+        }
+      }
+    ]
+  },
+
+  devtool: false,
   devServer: {
     static: './dist'
   }
